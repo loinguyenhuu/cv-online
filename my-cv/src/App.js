33f6b4fd475bcf39dd6880d3f70191e384a90084
loinguyenhuu <lyonguyen311@gmail.com>
2020-07-27 23:16:37 +0700

    create new project CV online

---
+++ my-cv/src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="sectionInfomation">
+      <div>
+        <img src="https://image.freepik.com/free-vector/abstract-dynamic-pattern-wallpaper-vector_53876-59131.jpg" />
+      </div>
+      <div className="infName">
+        <span> NGUYEN HUU LOI </span>
+      </div>
+      <div className="information">
+        <div>
+          <span> Date of birth: 08 / 01 / 1996 </span>
+          <span> Phone: 033 8783 293 </span>
+        </div>
+        <div>
+          <span> Email: lyonguyen311 @gmail.com </span>
+          <span> Facebook: /loi.nguyenhuu.731</span>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
