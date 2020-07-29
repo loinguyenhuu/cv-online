20b2437760175065c30da59d43b4ae769f9a96cd
loinguyenhuu <lyonguyen311@gmail.com>
2020-07-29 22:12:34 +0700

    update content education

--- my-cv/src/components/education/education.js
+++ my-cv/src/components/education/education.js
@@ -7,18 +7,9 @@ const Education = () => {
       <span className="title">Education</span>
       <div className="content">
         <div className="contenteducation">
-          Hi, I'm Pham Van Khanh, a Front-end developer with 3 years of
-          experience.
-          <br />
-          My main skills: ReactJS, Mobile-First Web Design
-          <br />
-          I especially like learning JavaScript (and it's libraries),
-          TypeScript, finding ways to optimize and increase performance
-          <br />
-          I like (deep) talking and chilling with other people. I was excited by
-          the colorful things.
-          <br />
-          My hobbies: reading, playing guitar, singing (but not really good)...
+          <p>University of Information Technology - VNU-HCM</p>
+          <li>Time: 2014 - 2019</li>
+          <li>Major: Software Engineering</li>
         </div>
       </div>
     </div>
