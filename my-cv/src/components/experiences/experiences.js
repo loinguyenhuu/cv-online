5d7c87915d8dd31c055244a0bc7afb01f5112114
loinguyenhuu <lyonguyen311@gmail.com>
2020-08-04 23:08:44 +0700

    create content experiences

--- my-cv/src/components/experiences/experiences.js
+++ my-cv/src/components/experiences/experiences.js
@@ -1,28 +1,110 @@
-import React from 'react';
-import './experiences.scss';
+import React from "react";
+import "./experiences.scss";
 
 const Experiences = () => {
-    return (
-        <div className="experiences">
-        <span className="title">Experiences</span>
-        <div className="content">
-            <div className="contentexperiences">
-          Hi, I'm Pham Van Khanh, a Front-end developer with 3 years of
-          experience.
-          <br />
-          My main skills: ReactJS, Mobile-First Web Design
-          <br />
-          I especially like learning JavaScript (and it's libraries), TypeScript,
-          finding ways to optimize and increase performance
-          <br />
-          I like (deep) talking and chilling with other people. I was excited by
-          the colorful things.
-          <br />
-          My hobbies: reading, playing guitar, singing (but not really good)...
-          </div>
+  return (
+    <div className="experiences">
+      <span className="title">Experiences</span>
+      <div className="content">
+        <div className="NTTDATA">
+          <p>NTT DATA VIET NAM</p>
+          <ul className="mb-0">
+            <li>Time: 05/2019 - Present</li>
+
+            <li>Role: Front-end developer</li>
+
+            <li>Technologies: ReactJS, HTML, CSS</li>
+
+            <li>
+              Development:
+              <ul>
+                <li>Worked task with git and merge request clearly.</li>
+                <li>
+                  Reviewed code before when create merge request and assign to
+                  PM or leader.
+                </li>
+                <li>
+                  Tested all bussiness system, feature, so have experience on
+                  test, as well as the test case for function or feature.
+                </li>
+                <li>Joined job fair with company, support member if need.</li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+        <div className="ZAMO">
+          <p>ZAMO</p>
+          <ul>
+            <li>Time: 10/2018 - 04/2019</li>
+
+            <li>Role: Fullstack developer </li>
+
+            <li>Technologies: Java </li>
+
+            <li>
+              Previous positions with Zamo LLC company:
+              <ul>
+                <li>
+                  My work experience included apply sprint in work task with
+                  team, estimate time complete task with team and leader.
+                </li>
+              </ul>
+            </li>
+            <li>
+              Development:
+              <ul>
+                <li>
+                  Wrote API back-end with java, used spring JPA, spring MVC.
+                </li>
+                <li>Wrote Front-end in project with handlebars and jquery.</li>
+                <li>
+                  Designed database and new design with the new model and UI.
+                </li>
+                <li>Learned basic Flutter Framework.</li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+        <div className="ORIENT">
+          <p>ORIENT SOFTWARE</p>
+          <ul>
+            <li>Time: 03/2018 - 06/2018</li>
+
+            <li>Role: Intern Fullstack developer </li>
+
+            <li>Technologies: Angular 4 and .NET Core</li>
+
+            <li>
+              Previous positions with Orient Software Development Corp. Vietnam:
+              <ul>
+                <li>
+                  My work experience included innovation in many areas,
+                  including strategies for a more effective teamwork. Designing
+                  wireframe and working closely with Director and Project
+                  Manager
+                </li>
+              </ul>
+            </li>
+            <li>
+              Development:
+              <ul>
+                <li>
+                  Contributed my ability to streamline teamwork processes with
+                  Github Issues and Visual Studio Online Tasks.
+                </li>
+                <li>How to build system real project.</li>
+                <li>Worked teamwork with SCRUM.</li>
+                <li>
+                  Knew to use Postman, Robo 3.T test code… Skill review code,
+                  work task with sprint…
+                </li>
+              </ul>
+            </li>
+          </ul>
         </div>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default Experiences;
